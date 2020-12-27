c27a2ed21335408939f0029cbcf3014ceb77dc72
Enrico Carraro <enrico.carraro@studenti.polito.it>
2020-12-27 18:17:39 +0100

    MVP

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
+
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [ready, setReady] = useState(false);
+  const [inputAudio, setInputAudio] = useState();
+  const [outputAudio, setOutputAudio] = useState();
+  const [stopThreshold, setStopThreshold] = useState(-50);
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  const cutSilence = async () => {
+    const outputName = (-stopThreshold) + "_silenced_" + inputAudio.name;
+    ffmpeg.FS('writeFile', inputAudio.name, await fetchFile(inputAudio));
+
+    console.log(`-i ${inputAudio.name} -af silenceremove=stop_periods=-1:stop_duration=1:stop_threshold=${stopThreshold}dB ${outputName}`);
+    await ffmpeg.run(
+      ...`-i ${inputAudio.name} -af silenceremove=stop_periods=-1:stop_duration=1:stop_threshold=${stopThreshold}dB ${outputName}`.split(
+        ' ',
+      ),
+    );
+    const data = ffmpeg.FS('readFile', outputName);
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'audio/mpeg' }),
+    );
+    setOutputAudio(url);
+  };
+
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
+    load();
+  }, []);
+
+
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <content>
+        {inputAudio && (
+          <audio width="250" controls>
+            <source src={URL.createObjectURL(inputAudio)} type="audio/mpeg" />
+            <> Your browser does not support the audio element. </>
+          </audio>
+        )}
+        <br />
+        <input
+          type="file"
+          onChange={(e) => setInputAudio(e.target.files?.item(0))}
+        />
+        <input
+          type="number"
+          placeholder="Sample value to be treated as silence in dB"
+          value={stopThreshold}
+          min="-1000"
+          max="-1"
+          onChange={(e) => setStopThreshold(e.target.value)}
+        />
+        <button onClick={inputAudio && cutSilence} disabled={!inputAudio}>
+          Cut Silence
+        </button>
+        <h3>Result</h3>
+        {outputAudio && (
+          <audio width="250" controls>
+            <source src={outputAudio} type="audio/mpeg" />
+            <> Your browser does not support the audio element. </>
+          </audio>
+        )}
+      </content>
     </div>
+  ) : (
+    <p> Loading </p>
   );
 }
 
